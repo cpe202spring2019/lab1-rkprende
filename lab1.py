fcfa105445ffff7fa6d7c37a39dc0426d907ef46
rkprende <49247787+rkprende@users.noreply.github.com>
2019-04-11 19:39:45 -0700

    fixed some test cases, correct None vs ValueError

--- lab1.py
+++ lab1.py
@@ -2,8 +2,10 @@
 def max_list_iter(int_list):  # must use iteration not recursion
    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   if len(int_list) == 0:
+   if int_list == None:
       raise ValueError('empty list')
+   if len(int_list) == 0:
+      return None
    max = int_list[0]
    for i in range(1, len(int_list)):
       if int_list[i] > max:
@@ -14,13 +16,13 @@ def max_list_iter(int_list):  # must use iteration not recursion
 def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   if len(int_list) == 0:
+   if int_list == None:
       raise ValueError('empty list')
    if len(int_list)== 1:
       return int_list
    else:
       temp = reverse_rec(int_list[1:])
-      return temp + temp.append(int_list[0])
+      return temp.append(int_list[0])
 
 
 def bin_search(target, low, high, int_list):  # must use recursion
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -4,10 +4,13 @@ from lab1 import *
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
-    def test_max_list_iter(self):
+    def test_max_list_iter_exception(self):                           # used to check for exception
         tlist = None
-        with self.assertRaises(ValueError):                 # used to check for exception
+        with self.assertRaises(ValueError):
             max_list_iter(tlist)
+    def test_max_list_iter_empty(self):                           #returns None is empty list
+        tlist = []
+        self.assertEqual(max_list_iter(tlist), None)
     def test_max_list_iter_all_positions(self):             #works if max is in any part of list
         tlist = [1,2,3]
         self.assertEqual(max_list_iter(tlist),3)
@@ -49,9 +52,14 @@ class TestLab1(unittest.TestCase):
 
 
 
-
+    def test_reverse_rec_exception(self):                           # used to check for exception
+        tlist = None
+        with self.assertRaises(ValueError):
+            reverse_rec(tlist)
+    def test_reverse_rec_empty(self):
+        tlist = []
+        self.assertEqual(reverse_rec(tlist), None)                  # don't think I need this one
     def test_reverse_rec(self):
-        #self.assertEqual(reverse_rec([]), ?)                               #Don't know how to check ValueError
         self.assertEqual(reverse_rec([1]), [1])                             #if only one item
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1])                      #reverse normal
         self.assertEqual(reverse_rec([-1,-2,-3]), [-3,-2,-1])               #reverse negatives
@@ -60,7 +68,11 @@ class TestLab1(unittest.TestCase):
 
 
 
-
+    def test_bin_search_empty(self):
+        list_val = []
+        low = 0
+        high = len(list_val)-1
+        self.asserEqual(bin_search(2, 0, len(list_val)-1, list_val), None)
     def test_bin_search_first_try(self):                                    #target on first try
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
@@ -70,7 +82,7 @@ class TestLab1(unittest.TestCase):
         list_val = [0, 1, 2, 3, 4, 7, 8, 9, 10]
         low = 0
         high = len(list_val) - 1
-        self.assertEqual(bin_search(8, 0, len(list_val) - 1, list_val), 8)
+        self.assertEqual(bin_search(8, 0, len(list_val) - 1, list_val), 6)
     def test_bin_search_smaller(self):                                      #target exists and smaller
         list_val = [0, 1, 2, 3, 4, 7, 8, 9, 10]
         low = 0
